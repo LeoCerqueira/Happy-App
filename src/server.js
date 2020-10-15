10c583e302543e8ac3f0757b8c9f994a1c36d663
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-10-15 15:51:28 -0300

    Fourth day

--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
 //import
 const express = require('express');
 const path = require('path')
+const pages = require('./pages.js')
 
 // starting library
 const server = express()
 server
+
 //using static files
     .use(express.static('public'))
 
     .set('views', path.join(__dirname, 'views'))
     .set('view engine','hbs')
-//creat a rote
-    .get('/' , (request, response) => {
-   
-   return response.render('index')
-   
-})
+
+//creata a rote
+    .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
 
 
 //Turn on the server 
