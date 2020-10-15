e6cb497ed7c61b3c0609f24b68ca619bb9c7f516
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-10-15 15:07:03 -0300

    hbs

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 //import
 const express = require('express');
+const path = require('path')
 
 // starting library
 const server = express()
+server
+//using static files
+    .use(express.static('public'))
 
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine','hbs')
 //creat a rote
-server.get('/' , () => {
+    .get('/' , (request, response) => {
+   
+   return response.render('index')
    
 })
+
 
 //Turn on the server 
 server.listen(5500)
