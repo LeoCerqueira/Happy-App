13208f68db7f197efd99bd113e99e28d94735e78
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-10-15 16:35:02 -0300

    Fourth dya

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+	{
+		id: 1,
+		lat: '-27.2196961',
+		lng: '-49.6494515',
+		name: 'Lar das meninas',
+		description:
+			'Presta assistência a criança de 06 a 15 anos que se encontre em situação de risco o/ou vulnerabilidade social.',
+		images: [
+			'https://images.unsplash.com/photo-1598322313658-512b47702270?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+			'https://images.unsplash.com/photo-1594753154778-273013529793?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+        ],
+        instuctions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours:"Horário de visitas Das 8 às 18",
+        open_on_weekends:"1"
+	},
+
+    {
+		id: 2,
+		lat: '-27.2196961',
+		lng: '-49.6594515',
+		name: 'Lar dos meninos',
+		description:
+			'Presta assistência a criança de 06 a 15 anos que se encontre em situação de risco o/ou vulnerabilidade social.',
+		images: [
+			'https://images.unsplash.com/photo-1598322313658-512b47702270?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+			'https://images.unsplash.com/photo-1594753154778-273013529793?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+        ],
+        instuctions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours:"Horário de visitas Das 8 às 18",
+        open_on_weekends:"0"
+	}
+];
