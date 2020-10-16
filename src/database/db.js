71f140fe61c1cadb7d1cd42d709d32c1fb884109
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-10-16 10:09:58 -0300

    sqlite

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
-const Database = require('sqlite-async');
+const Database = require('sqlite-async')
 
-Database.open(__dirname + '/database.sqlite').then(execute)
+open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db){
     console.log('entrei nessa função')
