f2314e237fe0328d98aa368025c368bbc8def6a1
Leandro Cerqueira Santos <ll_cerqueira@hotmail.com>
2020-10-16 11:19:57 -0300

    creating a database

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,9 @@
-const Database = require('sqlite-async')
+const Database = require('sqlite-async');
 
-open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db){
-    console.log('entrei nessa função')
-
-  /*  de.exec(`
+   
+   return db.exec(`
         CREATE TABLE IF NOT EXISTS orphanages (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             lat TEXT,
@@ -18,5 +16,7 @@ function execute(db){
             opening_hours TEXT,
             open_on_weekends TEXT
         );
-    `) */
+    `) 
 }
+
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute) // db
